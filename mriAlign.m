6b989ed6d2b3dcfca95af58d0a82279ce6335af4
Ke Zeng <33518354+headfirstK@users.noreply.github.com>
2018-12-30 12:42:18 -0500

    Update mriAlign.m

--- mriAlign.m
+++ mriAlign.m
@@ -1,25 +1,32 @@
 
+% This file is used to align the MRI to the 'neuromag' coordinate system.
 
 clear;clc
 
-%% add the path of fieldtrip
+%% Add the path of fieldtrip
 fieldtrip_path = '/Users/ke/toolbox/fieldtrip-20180719';
 addpath(fieldtrip_path,'-end');
 ft_defaults
 
-%% load data
+
+%% Root directory of MRI file
 mri_root_path = '/Users/ke/OneDrive - UHN/MRI';
 
+% Subpath of PD patients
 % group = 'PD';
 % subjects = {'PD_A', 'PD_D', 'PD_E', 'PD_F', 'PD_G', 'PD_H', 'PD_J', 'PD_K', 'PD_L','PD_M', 'PD_O', 'PD_P', 'PD_Q', 'PD_R'};
 % states = {'On', 'Off'};
 
+% Subpath of health controls
 group = 'OC';
 subjects = {'OC_B','OC_C','OC_D','OC_F','OC_G','OC_H','OC_J','OC_K','OC_L','OC_N','OC_O','OC_P','OC_Q','OC_R','OC_S'};
 states = {''};
 
+% The file name of MRI
 file_name = 'mri_raw.mat';
 
+
+%% Align MRI for each subject during each state
 for i = 1:length(subjects)    
     subject = subjects{i};
     fprintf('%s\n', subject);
@@ -30,28 +37,32 @@ for i = 1:length(subjects)
         mri_data_file = fullfile(mri_root_path, group, subject, state, file_name);
         load(mri_data_file);
         
-        %% convert coordinate from original to acpc
+        % Convert coordinate from original to 'acpc' coordinate
         cfg = [];
         cfg.method = 'interactive';
         cfg.coordsys = 'acpc';
         mri_acpc    = ft_volumerealign(cfg, mri_raw);
         
-        %% reslice mri
+        % Reslice mri
         cfg            = [];
-        mri_rs     = ft_volumereslice(cfg, mri_acpc);
+        mri_rs     = ft_volumereslice(cfg, mri_acpc);        
         
-%         mri_rs = mri_raw;
+        % Align MRI to Nasion/LPC/RPC
+        stop = 'n';
+        while (stop ~= 'y')
         
-        %%
-        char = 'n';
-        while (char ~= 'y')
+            cfg = []
             cfg.method = 'interactive';
             cfg.coordsys = 'neuromag';
             [mri_align] = ft_volumerealign(cfg, mri_rs)
+            
+            % Check the alignment resluts
             [dataout] = ft_determine_coordsys(mri_align);
-            char = input('ending?:','s');
+            
+            stop = input('ending?:','s');
         end
         
+        % Save the aligned MRI
         save(fullfile(mri_root_path, group, subject, state, 'mri_align.mat'), 'mri_align');
     end
 end
