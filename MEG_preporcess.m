228fbca80abb798e467a6a81e39e94037d29c61e
Ke Zeng <33518354+headfirstK@users.noreply.github.com>
2019-01-02 10:50:32 -0500

    Update MEG_preporcess.m

--- MEG_preporcess.m
+++ MEG_preporcess.m
@@ -1,4 +1,5 @@
 
+% This file is used to preprocess the raw MEG data.
 
 clear;clc;
 
@@ -7,21 +8,22 @@ fieldtrip_path = '/Users/ke/toolbox/fieldtrip-20180719';
 addpath(fieldtrip_path,'-end');
 ft_defaults
 
-%% Prepare data file
+%% Root directory of MEG file
 meg_root_path = '/Users/ke/OneDrive - UHN/MEG';
 
+% Subpath of PD patients
 % group = 'PD';
 % subjects = {'PD_A', 'PD_D', 'PD_E', 'PD_F', 'PD_G', 'PD_H', 'PD_K', 'PD_L','PD_M', 'PD_O', 'PD_P', 'PD_Q', 'PD_R'};
 % states = {'Off', 'On'};
 
+% Subpath of health controls
 group = 'OC';
 subjects = {'OC_B', 'OC_C', 'OC_D', 'OC_F', 'OC_G', 'OC_H', 'OC_K', 'OC_L', 'OC_N', 'OC_O', 'OC_P', 'OC_Q', 'OC_R', 'OC_S'};
 states = {''};
 
-
 file_name = 'ECLOS_tsss_mc.fif';
 
-%% Preprocess
+%% Preprocess raw MEG data for each subject during each state
 for i = 1:length(subjects)
     subject = subjects{i};
     fprintf('%s\n', subject);
@@ -34,12 +36,13 @@ for i = 1:length(subjects)
         % Read the data from disk and segment it into 5-second pieces
         cfg = [];
         cfg.dataset                 = meg_data_file;
-        cfg.channel                 = {'MEGGRAD'};
+        cfg.channel                 = {'MEGGRAD'};          % Only use gradient channel
         cfg.trialfun                = 'ft_trialfun_general';
-        cfg.trialdef.triallength    = 8;                 % duration in seconds
-        cfg.trialdef.ntrials        = inf;               % number of trials, inf results in as many as possible
+        cfg.trialdef.triallength    = 8;                    % duration in seconds
+        cfg.trialdef.ntrials        = inf;                  % number of trials, inf results in as many as possible
         cfg                         = ft_definetrial(cfg);
         
+        % Filt the data and remove line noise
         cfg.bsfilter        = 'yes';
         cfg.bsfreq          = [57 63;117 123;177 183];      
         cfg.bsfiltord       = 4; 
@@ -51,10 +54,7 @@ for i = 1:length(subjects)
         % Reject bad trials
         cfg             = [];
         cfg.method      = 'trial';
-        %cfg.channel     = 'MEG';
         cfg.alim        = 5e-11;
-        % cfg.gradscale   = 1;
-        % cfg.magscale    = 1/0.04;
         meg_clean1   = ft_rejectvisual(cfg, meg_trials);
         
         % Reject bad sensors
